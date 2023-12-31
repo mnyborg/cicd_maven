e897844fc1926652000d6de18626fd3f038fe99b
Mads Nyborg <MANY@kea.dk>
2023-12-31 13:58:57 +0100

    fremprovokeret fejl i unit test

--- src/test/java/com/example/cicd_maven/CicdMavenApplicationTests.java
+++ src/test/java/com/example/cicd_maven/CicdMavenApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.cicd_maven;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class CicdMavenApplicationTests {
 
     @Test
     void contextLoads() {
+        fail();
     }
 
 }
